57d51d04a2ad54a054d82326af8e58f7467cc2f6
Matti Sangi <matti.sangi@pp.inet.fi>
2015-01-22 13:26:09 +0200

    v2.0.1 - button click handling moved completely to js

--- public/javascript/abook.js
+++ public/javascript/abook.js
@@ -3,63 +3,7 @@ var abook = {
     renderBody: function(data) {
         $('.container').replaceWith(data);
         $('.container').trigger('create');
-    },
-    clickAddContact: function() {
-        $.ajax({
-            type: 'GET',
-            url: '/add_contact',
-            success: function(data) {
-                abook.renderBody(data);
-            }
-        })
-    },
-    clickContactAdd: function() {
-        $.ajax({
-            type: 'POST',
-            url: '/add_contact',
-            data: {
-                id: $('#id').val(),
-                name: $('#name').val(),
-                address: $('#address').val() ,
-                email: $('email').val(),
-                phone: $('phone').val(),
-                birthday: $('birthday').val(),
-                otherInfo: $('otherInfo').val()
-            },
-            success: function(data) {
-                abook.renderBody(data);
-            }
-        })
-    },
-    clickContactBack: function() {
-        $.ajax({
-            type: 'GET',
-            url: '/contacts',
-            success: function(data) {
-                abook.renderBody(data);
-            }
-        })
-    },
-    clickContactCancel: function() {
-        $.ajax({
-            type: 'GET',
-            url: '/contacts',
-            success: function(data) {
-                abook.renderBody(data);
-            }
-        })
-    },
-    clickContactDelete: function() {
-        $.ajax({
-            type: 'DELETE',
-            url: '/delete_contact',
-            data: {
-                id: $('#id').val()
-            },
-            success: function(data) {
-                abook.renderBody(data);
-            }
-        });
+        abook.registerListeners();
     },
     clickContactShow: function(id) {
         $.ajax({
@@ -73,76 +17,138 @@ var abook = {
             }
         })
     },
-    clickContactUpdate: function() {
-        $.ajax({
-            type: 'POST',
-            url: '/update_contact',
-            data: {
-                id: $('#id').val(),
-                name: $('#name').val(),
-                address: $('#address').val() ,
-                email: $('email').val(),
-                phone: $('phone').val(),
-                birthday: $('birthday').val(),
-                otherInfo: $('otherInfo').val()
-            },
-            success: function(data) {
-                abook.renderBody(data);
-            }
-        })
-    },
-    clickLogin: function() {
-        $.ajax({
-            type: 'POST',
-            url: '/login',
-            data: {
-                username: $('#username').val(),
-                password: $('#password').val()
-            },
-            success: function(data) {
-                abook.renderBody(data);
-            }
+    registerListeners: function() {
+        $('#button-contact-add').click(function() {
+            $.ajax({
+                type: 'POST',
+                url: '/add_contact',
+                data: {
+                    id: $('#id').val(),
+                    name: $('#name').val(),
+                    address: $('#address').val() ,
+                    email: $('email').val(),
+                    phone: $('phone').val(),
+                    birthday: $('birthday').val(),
+                    otherInfo: $('otherInfo').val()
+                },
+                success: function(data) {
+                    abook.renderBody(data);
+                }
+            });
         });
-    },
-    clickLogout: function() {
-        $.ajax({
-            type: 'GET',
-            url: '/logout',
-            success: function(data) {
-                abook.renderBody(data);
-            }
-        })
-    },
-    clickRegister: function() {
-        $.ajax({
-            type: 'GET',
-            url: '/register',
-            success: function(data) {
-                abook.renderBody(data);
-            }
+        $('#button-contact-add-new').click(function() {
+            $.ajax({
+                type: 'GET',
+                url: '/add_contact',
+                success: function(data) {
+                    abook.renderBody(data);
+                }
+            });
         });
-    },
-    clickRegisterForm: function() {
-        $.ajax({
-            type: 'POST',
-            url: '/register',
-            data: {
-                username: $('#username').val(),
-                password: $('#password').val(),
-                email: $('#email').val()
-            },
-            success: function(data) {
-                abook.renderBody(data);
-            }
+        $('#button-contact-back').click(function() {
+            $.ajax({
+                type: 'GET',
+                url: '/contacts',
+                success: function(data) {
+                    abook.renderBody(data);
+                }
+            });
         });
-    },
-    clickRegisterCancel: function() {
-        $.ajax({
-            type: 'GET',
-            url: '/login',
-            success: function(data) {
-                abook.renderBody(data);
-            }
+        $('#button-contact-cancel').click(function() {
+            $.ajax({
+                type: 'DELETE',
+                url: '/delete_contact',
+                data: {
+                    id: $('#id').val()
+                },
+                success: function(data) {
+                    abook.renderBody(data);
+                }
+            });
+        });
+        $('#button-contact-delete').click(function() {
+            $.ajax({
+                type: 'DELETE',
+                url: '/delete_contact',
+                data: {
+                    id: $('#id').val()
+                },
+                success: function(data) {
+                    abook.renderBody(data);
+                }
+            });
+        });
+        $('#button-contact-update').click(function() {
+            $.ajax({
+                type: 'POST',
+                url: '/update_contact',
+                data: {
+                    id: $('#id').val(),
+                    name: $('#name').val(),
+                    address: $('#address').val() ,
+                    email: $('email').val(),
+                    phone: $('phone').val(),
+                    birthday: $('birthday').val(),
+                    otherInfo: $('otherInfo').val()
+                },
+                success: function(data) {
+                    abook.renderBody(data);
+                }
+            });
+        });
+        $('#button-login').click(function() {
+            $.ajax({
+                type: 'POST',
+                url: '/login',
+                data: {
+                    username: $('#username').val(),
+                    password: $('#password').val()
+                },
+                success: function(data) {
+                    abook.renderBody(data);
+                }
+            });
+        });
+        $('#button-logout').click(function() {
+            $.ajax({
+                type: 'GET',
+                url: '/logout',
+                success: function(data) {
+                    abook.renderBody(data);
+                }
+            });  
+        });
+        $('#button-register').click(function() {
+            $.ajax({
+                type: 'GET',
+                url: '/register',
+                success: function(data) {
+                    abook.renderBody(data);
+                }
+            });
+        });
+        $('#button-register-cancel').click(function() {
+            $.ajax({
+                type: 'GET',
+                url: '/login',
+                success: function(data) {
+                    abook.renderBody(data);
+                }
+            });
+        });
+        $('#button-register-form').click(function() {
+            $.ajax({
+                type: 'POST',
+                url: '/register',
+                data: {
+                    username: $('#username').val(),
+                    password: $('#password').val(),
+                    email: $('#email').val()
+                },
+                success: function(data) {
+                    abook.renderBody(data);
+                }
+            });
         });
     }
 };
